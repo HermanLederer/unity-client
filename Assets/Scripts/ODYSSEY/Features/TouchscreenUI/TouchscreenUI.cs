a617216b99cf3d105f9fef894aac3a45b074450e
Herman Lederer <germans.lederers@gmail.com>
2022-06-08 13:27:00 +0200

    Rename Thumbstick to TouchscreenUI and implement visibility toggling

---
+++ Assets/Scripts/ODYSSEY/Features/TouchscreenUI/TouchscreenUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchscreenUI : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField]
+    GameObject Viewport;
+
+    public TouchscreenUIDriver Driver { get; set; }
+
+    void Awake()
+    {
+        SetVisibility(false);
+    }
+
+    public void SetVisibility(bool visibility)
+    {
+        Viewport.gameObject.SetActive(visibility);
+    }
+
+    public void ToggleVisibility()
+    {
+        SetVisibility(!Viewport.gameObject.activeSelf);
+    }
+}
---
+++ Assets/Scripts/ODYSSEY/Features/TouchscreenUI/TouchscreenUIDriver.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Odyssey;
+
+public interface ITouchscreenUIDriver
+{
+    // void ShowHideMinimap(bool show);
+    // bool IsExpanded();
+    // void Clear();
+}
+
+public class TouchscreenUIDriver : MonoBehaviour, IRequiresContext, ITouchscreenUIDriver
+{
+    [Header("References")]
+    [SerializeField]
+    TouchscreenUI touchscreenUI;
+
+    void OnEnable()
+    {
+        touchscreenUI.Driver = this;
+        _c.Get<IReactBridge>().ToggleTouchscreenUI_Event += OnToggleTouchscreenUI;
+    }
+
+    void OnDisable()
+    {
+        touchscreenUI.Driver = null;
+        _c.Get<IReactBridge>().ToggleTouchscreenUI_Event -= OnToggleTouchscreenUI;
+    }
+
+    public void Init(IMomentumContext context)
+    {
+        _c = context;
+    }
+
+    #region Event Handlers
+
+    [ContextMenu("Toggle Minimap")]
+    private void OnToggleTouchscreenUI()
+    {
+        touchscreenUI.ToggleVisibility();
+    }
+
+    #endregion
+
+    IMomentumContext _c;
+}
